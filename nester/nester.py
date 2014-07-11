582fdfa6536307aa7e68d66c25ef6c7091c66271
Leonardo Barros <nickolback@gmail.com>
2014-07-10 22:42:52 -0300

    Alterando o print_lol do if com level+1

--- nester/nester.py
+++ nester/nester.py
@@ -13,7 +13,7 @@ def print_lol(the_list, level):
 
   for each_item in the_list:
     if isinstance(each_item, list):
-      print_lol(each_item)
+      print_lol(each_item, level+1)
     else:
       for tab_stop in range(level):
         print('\t'+ '') 
