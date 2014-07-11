5d5e88c161a8fe9c6218eff6b7f1bf09505dd4f3
Leonardo Barros <nickolback@gmail.com>
2014-07-10 21:42:37 -0300

    Adicioanndo mais um parametro no modulo nester

--- nester/nester.py
+++ nester/nester.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """Módulo para visuzalizar listas aninhadas. 
    Módulo nester.py"""
 
-def print_lol(the_list):
+def print_lol(the_list, level):
   """Função para percorrer a lista e mostrar na tela.
      Tem a função print_lol usando isinstance para 
-     verificar ser é uma lista"""
+     verificar ser é uma lista
+     Um segund argumento chamado "level" é usado para inserer tabulação
+     quando uma lista aninhada é encontrada"""
 
   for lista_item in the_list:
     if isinstance(lista_item, list):
       print_lol(lista_item)
     else:
+      for tab_stop in range(level):
+        print("\t", '')
+
       print(lista_item)
