e7b825015dd14c61fb131d54b4bd9989622fea82
Leonardo Barros <nickolback@gmail.com>
2014-07-12 12:56:08 -0300

    Adicionando um novo argumento em each_line.split()

--- sketch.py
+++ sketch.py
@@ -3,12 +3,13 @@
 
 import os
 
+#pega o caminho do seu diretorio
 os.chdir('/home/leonardo/python/use_cabeca_python/hfpy_code/chapter3')
 
 data = open('sketch.txt')
 
 for each_line in data:
-  (role, line_spoken) = each_line.split(':')
+  (role, line_spoken) = each_line.split(':', 1)
   print(role, end='')
   print(' said: ', end='')
   print(line_spoken, end='')
