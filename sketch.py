c519b2854182e46bea4e655291fff1def566d6af
Leonardo Barros <nickolback@gmail.com>
2014-07-19 20:09:06 -0300

    Adicioando o try e except no arquivo

--- sketch.py
+++ sketch.py
@@ -9,10 +9,13 @@ os.chdir('/home/leonardo/python/use_cabeca_python/hfpy_code/chapter3')
 data = open('sketch.txt')
 
 for each_line in data:
-  if not each_line.find(':') == -1:
-    (role, line_spoken) = each_line.split(':', 1)
-    print(role, end='')
-    print(' said: ', end='')
-    print(line_spoken, end='')
+  try:
+    if not each_line.find(':') == -1:
+      (role, line_spoken) = each_line.split(':', 1)
+      print(role, end='')
+      print(' said: ', end='')
+      print(line_spoken, end='')
+  except:
+      pass
 
 data.close()
