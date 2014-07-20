65eaf1e01a9bfabace50a8b6cfaf1f69b92ebc67
Leonardo Barros <nickolback@gmail.com>
2014-07-20 07:09:08 -0300

    Arquivos baseados no capitulo 3 do livro
    Foi criado o programa ler.py para carregar o texto.

---
+++ aprendendo/ler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: uTF-8 -*-
+
+import os
+
+os.chdir('/home/leonardo/python/use_cabeca_python/aprendendo')
+try:
+  arquivo = open('einstein.txt')
+  
+  for linha in arquivo:
+    print(linha, end='')
+
+except IOError:
+  print("Não foi possível abrir o arquivo")
+
+
