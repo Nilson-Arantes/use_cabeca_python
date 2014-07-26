6a2f918782f0a4707eeac0ce3ecbffa5bb750297
Leonardo Barros <nickolback@gmail.com>
2014-07-26 19:22:14 -0300

    Colocado o finally para encerrar os arquivos
    O finally vai executar sempre, mesmo que aconteça algo de errado

--- cap4/sketch2.py
+++ cap4/sketch2.py
@@ -27,9 +27,10 @@ try:
     other_file = open('other_data.txt', 'w')
     print(man, file=man_file)
     print(other, file=other_file)
-
-    man_file.close()
-    other_file.close()
+    
 except IOError:
     print('File error')
 
+finally:
+    man_file.close()
+    other_file.close()
